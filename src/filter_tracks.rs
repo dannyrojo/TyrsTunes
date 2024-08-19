ac7de3728f25caaac2ea64aa40f09b8f55c41b95
dannyred <dannyred365+github@gmail.com>
2024-08-19 04:37:24 -0700

    filter tracks by mode

--- src/filter_tracks.rs
+++ src/filter_tracks.rs
@@ -1,11 +1,34 @@
 use crate::import_tracks::Track;
+use itertools::Itertools; // Added this import at the top of the file
 
-pub fn filter_tracks_by_comment(tracks: Vec<Track>, filter: &str) -> Vec<Track> {
-    tracks.into_iter().filter(|track| track.comments_vec.contains(&filter.to_string())).collect()
+pub enum FilterMode {
+    And,
+    Or,
+    Both,
 }
 
-pub fn get_all_comments(tracks: Vec<Track>) -> Vec<String> {
-    tracks.into_iter().flat_map(|track| track.comments_vec).collect()
+pub fn filter_tracks_by_comments(tracks: Vec<Track>, filters: &[&str], mode: FilterMode) -> Vec<Track> {
+    tracks.into_iter().filter(|track| {
+        match mode {
+            FilterMode::And => filters.iter().all(|&filter| track.comments_vec.contains(&filter.to_string())),
+            FilterMode::Or => filters.iter().any(|&filter| track.comments_vec.contains(&filter.to_string())),
+            FilterMode::Both => {
+                let and_result = filters.iter().all(|&filter| track.comments_vec.contains(&filter.to_string()));
+                let or_result = filters.iter().any(|&filter| track.comments_vec.contains(&filter.to_string()));
+                and_result || or_result
+            }
+        }
+    }).collect()
+}
+
+pub fn get_all_comments_unique(tracks: Vec<Track>) -> Vec<String> {
+    let mut comments: Vec<String> = tracks
+        .into_iter()
+        .flat_map(|track| track.comments_vec)
+        .unique()
+        .collect();
+    comments.sort();
+    comments
 }
 
 #[cfg(test)]
@@ -14,20 +37,18 @@ mod tests {
     use crate::import_tracks;
 
     #[test]
-    fn test_filter_tracks_by_comment() {
+    fn test_filter_tracks_by_comments() {
         let tracks = import_tracks::import_directory("/home/eggbert/songs");
-        let filtered_tracks = filter_tracks_by_comment(tracks, "Moody");
-        assert_eq!(filtered_tracks.len(), 2);
+        let filtered_tracks = filter_tracks_by_comments(tracks, &["Energetic", "Atmospheric"], FilterMode::And);
+        assert_eq!(filtered_tracks.len(), 1);
     }
 
+
     #[test]
-    fn test_get_all_comments() {
+    fn test_get_all_comments_unique() {
         let tracks = import_tracks::import_directory("/home/eggbert/songs");
-        let comments = get_all_comments(tracks);
-        let expected_comments = vec!["Moody", "Atmospheric", "Exciting", "Energetic"];
-        
-        for comment in expected_comments {
-            assert!(comments.contains(&comment.to_string()));
-        }
+        let comments = get_all_comments_unique(tracks);
+        let expected_comments = vec!["Atmospheric", "Energetic", "Exciting", "Moody"];
+        assert_eq!(comments, expected_comments);
     }
 }
