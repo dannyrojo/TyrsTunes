76496e3aecdfbc73bd2e1affce8670ed75605f34
dannyred <dannyred365+github@gmail.com>
2024-08-20 22:05:16 -0700

    basic gui template

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,46 @@ impl AppWindow {
 }
 
 impl eframe::App for AppWindow {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Tyrs Tunes");
-        });
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let panel_height = ctx.available_rect().height() / 2.0;
+        let panel_width = ctx.available_rect().width() / 2.0;
+
+        // Top Panels
+        egui::TopBottomPanel::top("top_panel")
+            .min_height(panel_height)
+            .show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    // Top Left Panel
+                    ui.vertical(|ui| {
+                        ui.set_width(panel_width);
+                        ui.label("Tracks");
+                    });
+
+                    // Top Right Panel
+                    ui.vertical(|ui| {
+                        ui.set_width(panel_width);
+                        ui.label("Filter");
+                    });
+                });
+            });
+
+        // Bottom Panels
+        egui::TopBottomPanel::bottom("bottom_panel")
+            .min_height(panel_height)
+            .show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    // Bottom Left Panel
+                    ui.vertical(|ui| {
+                        ui.set_width(panel_width);
+                        ui.label("Playlist 1");
+                    });
+
+                    // Bottom Right Panel
+                    ui.vertical(|ui| {
+                        ui.set_width(panel_width);
+                        ui.label("Playlist 2");
+                    });
+                });
+            });
     }
 }
